6e795b4c743d5ac9e6bde9d6d0dceb27c817c8a4
Jun Hatori <hatori@preferred.jp>
2021-10-16 17:03:11 +0900

    main2

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python3
 """Python script to test GitHub actions"""
 import sys
+from typing import Optional
 
 
 def main() -> int:
     """main"""
     result = 1 + 1
     sys.stdout.write(f"Test {result}\n")
+    return result
+
+
+def main2() -> Optional[int]:
+    """main2"""
+    result = 3
     return result
 
 
--- test_main.py
+++ test_main.py
@@ -5,3 +5,8 @@ import main
 def test_main() -> None:
     """test_main"""
     assert main.main() == 2
+
+
+def test_main2() -> None:
+    """test_main2"""
+    assert main.main2() == 3
